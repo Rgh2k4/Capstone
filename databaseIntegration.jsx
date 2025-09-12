e72307fe2154bf24a40f8dbcdacd10c50bc602f0
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2025-09-11 20:46:39 -0600

    Very base code of database integration

---
+++ databaseIntegration.jsx
@@ -0,0 +1,25 @@
+//The base for this code is directly provided by Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRUZuFSYX5eft2eMjp6PH45LBkNbrrWCI",
+  authDomain: "capstone-ca0fe.firebaseapp.com",
+  databaseURL: "https://capstone-ca0fe-default-rtdb.firebaseio.com",
+  projectId: "capstone-ca0fe",
+  storageBucket: "capstone-ca0fe.firebasestorage.app",
+  messagingSenderId: "230065401291",
+  appId: "1:230065401291:web:1c8c06b155824ba4cf5a4e",
+  measurementId: "G-KW1YHK2ZL8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
