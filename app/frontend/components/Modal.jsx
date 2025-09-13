57af3b78a4a317d432b92856543588e6e93dbda9
Amani Bera <141461158+amanibera-sait@users.noreply.github.com>
2025-09-12 23:11:36 -0600

    Added styling to the Modal component and a link of the code I referenced.

--- app/frontend/components/Modal.jsx
+++ app/frontend/components/Modal.jsx
@@ -1,16 +1,26 @@
+/*
+
+The youtube video I referenced for this code: https://www.youtube.com/watch?v=nwJK-jo91vA
+
+*/
+
 const Modal = ({isVisible, onClose, children}) => {
   if (!isVisible) return null;
 
   const handleClose = (e) => {
-    if ( e.target.id === 'wrapper' ) onClose();
+    if ( e.target.id == 'wrapper' ) onClose();
   }
   
   return (
-    <div className=' fixed inset-0 bg-black  bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id='wrapper' onClick={() => handleClose()}>
-      <div className=' w-[600px]'>
+    <div className=' fixed inset-0 backdrop-opacity-35 backdrop-brightness-0 flex justify-center items-center' id='wrapper' onClick={() => handleClose()}>
+      <div className=' max-w-1/2 max-h-screen overflow-y-scroll'>
         <div className=' bg-white p-2 rounded'>
-          <button className=' bg-blue-300 rounded-b-md absolute p-6 m-6' onClick={() => onClose()}>X</button>
-          {children}
+          <div className="absolute left-2/3">
+            <button className='modal-close' onClick={() => onClose()}>X</button>
+          </div>
+          <div>
+            {children}            
+          </div>
         </div>
       </div>
     </div>
