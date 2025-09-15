a86eb3e3593879b9ebde83f9bb087bfd30f43126
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-09-15 13:56:06 -0600

    Added a secondary Modal tag that contains the Reports. Delete and Approve buttons now close the current Modal.

--- app/frontend/admin_menu.jsx
+++ app/frontend/admin_menu.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import ReviewList from "./components/admin/reviews/review_list";
 import Modal from "./components/Modal";
 import ReviewWindow from "./components/admin/reviews/review_window";
+import ReportList from "./components/admin/reports/report_list";
+import ReportWindow from "./components/admin/reports/report_window";
 
 function AdminMenu() {
   const [showModal, setShowModal] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
   const [user, setUser ] = useState();
   const [pageName, setPageName] = useState("Dashboard")
   
@@ -24,11 +27,13 @@ function AdminMenu() {
         <h1 className="text-4xl break-normal font-bold text-white text-shadow-lg text-shadow-black text-center">
           {pageName}
         </h1>
-        <div>
+        <div className="flex flex-row space-x-4">
           <ReviewList setShowModal={setShowModal} sendUser={setUser}/>
+          <ReportList setShowModal={setShowModal2} sendUser={setUser}/>
         </div>
       </section>
-      <Modal isVisible={showModal} onClose={() => setShowModal(false)}><ReviewWindow user={user}/></Modal>
+      <Modal isVisible={showModal} onClose={() => setShowModal(false)}><ReviewWindow onClose={() => setShowModal(false)} user={user}/></Modal>
+      <Modal isVisible={showModal2} onClose={() => setShowModal2(false)}><ReportWindow onClose={() => setShowModal2(false)} user={user}/></Modal>
     </div>
   );
 }
