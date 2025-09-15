e9d59035a0899297224d8429260606adfa76b45c
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-09-15 13:52:19 -0600

    Changed the text size for the list of submissions.

--- app/frontend/components/admin/reviews/review_submissions.jsx
+++ app/frontend/components/admin/reviews/review_submissions.jsx
@@ -14,10 +14,10 @@ function ReviewSubmissions( { handleReview }) {
       {reviews.map((rev, index) => (
           <div key={index} className="border-b-4 border-gray-300">
             <div className="grid grid-cols-3 gap-4 m-4">
-              <p className=" mt-6 mr-8 font-semibold text-3xl">
+              <p className=" mt-6 mr-8 font-semibold text-2xl">
                 {rev.username}
               </p>
-              <p className=" mt-6 mr-8 text-2xl">
+              <p className=" mt-6 mr-8 text-1xl">
                 Submited: {rev.date}
               </p>
               <button onClick={() => handleData({rev})}>
