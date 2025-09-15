f06179c5e4b344c93f36ab9c3e9e51d55fc8501f
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-09-15 13:54:13 -0600

    Created the Report components to the admin  page.

---
+++ app/frontend/components/admin/reports/report_list.jsx
@@ -0,0 +1,25 @@
+import ReportSubmissions from './report_submissions';
+
+function ReportList( {setShowModal, sendUser}) {
+
+  const handleReport = (user) => {
+    console.log("Data Recieved:");
+    console.log(user);
+    sendUser(user);
+    setShowModal(true);
+  }
+
+
+  return (
+    <section className=' bg-gray-100 drop-shadow-md drop-shadow-gray-400'>
+        <div className='flex justify-center text-4xl font-bold bg-gray-300 p-4 space-y-4 overflow-y-auto drop-shadow-sm drop-shadow-gray-400'>
+          <p>Reports</p>
+        </div>
+        <div className="rounded-b-lg p-4 overflow-y-auto" >
+          <ReportSubmissions handleReport={handleReport}/>
+        </div>
+    </section>
+  )
+}
+
+export default ReportList
