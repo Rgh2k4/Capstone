48568e94045c8929025fdd5dce8d87f398cfa8e5
Amani Bera <141461158+amanibera-sait@users.noreply.github.com>
2025-09-15 20:49:39 -0600

    Formated the code.

--- app/frontend/components/admin/accounts/add_window.jsx
+++ app/frontend/components/admin/accounts/add_window.jsx
@@ -1,5 +1,4 @@
-function Add({onClose, role, setRole}) {
-
+function Add({ onClose, role, setRole }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -11,21 +10,38 @@ function Add({onClose, role, setRole}) {
   }
 
   return (
-    <div className=' p-12 rounded flex flex-col justify-center text-center space-y-24'>
-        <div className='flex justify-center text-center text-4xl font-bold'>
-            <p>Add {role}</p>
-        </div>
-        <div className='flex flex-col space-y-6'>
-            <input value={username} onChange={setUsername} type="text" placeholder="Enter Username..." className="input"/>
-            <input value={password} onChange={setPassword} type="text" placeholder="Enter Password..." className="input"/>
-            <input value={email} onChange={setEmail} type="text" placeholder="Enter Email..." className="input"/>
-        </div>
-        <div>
-            <button onClick={handleAdd}>Add</button>
-        </div>
+    <div className=" p-12 rounded flex flex-col justify-center text-center space-y-24">
+      <div className="flex justify-center text-center text-4xl font-bold">
+        <p>Add {role}</p>
+      </div>
+      <div className="flex flex-col space-y-6">
+        <input
+          value={username}
+          onChange={setUsername}
+          type="text"
+          placeholder="Enter Username..."
+          className="input"
+        />
+        <input
+          value={password}
+          onChange={setPassword}
+          type="text"
+          placeholder="Enter Password..."
+          className="input"
+        />
+        <input
+          value={email}
+          onChange={setEmail}
+          type="text"
+          placeholder="Enter Email..."
+          className="input"
+        />
+      </div>
+      <div>
+        <button onClick={handleAdd}>Add</button>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Add
+export default Add;
--- app/frontend/components/admin/reports/report_submissions.jsx
+++ app/frontend/components/admin/reports/report_submissions.jsx
@@ -1,33 +1,27 @@
 import users from "../user_test_data.json";
 
-function ReviewSubmissions( { handleReport }) {
-  let reviews = users.reports;
+function ReportSubmissions({ handleReport }) {
+  let reports = users.reports;
 
-  function handleData({ rev }) {
+  function handleData({ rep }) {
     console.log("Data:");
-    console.log(rev);
-    
-    handleReport(rev);
+    console.log(rep);
+
+    handleReport(rep);
   }
   return (
     <ul>
-      {reviews.map((rev, index) => (
-          <div key={index} className="border-b-4 border-gray-300">
-            <div className="grid grid-cols-3 gap-4 m-4">
-              <p className=" mt-6 mr-8 font-semibold text-2xl">
-                {rev.username}
-              </p>
-              <p className=" mt-6 mr-8 text-1xl">
-                Submited: {rev.date}
-              </p>
-              <button onClick={() => handleData({rev})}>
-                Review
-              </button>
-            </div>
+      {reports.map((rep, index) => (
+        <div key={index} className="border-b-4 border-gray-300">
+          <div className="grid grid-cols-3 gap-4 m-4">
+            <p className=" mt-6 mr-8 font-semibold text-2xl">{rep.username}</p>
+            <p className=" mt-6 mr-8 text-1xl">Submited: {rep.date}</p>
+            <button onClick={() => handleData({ rep })}>Review</button>
           </div>
+        </div>
       ))}
     </ul>
   );
 }
 
-export default ReviewSubmissions;
+export default ReportSubmissions;
