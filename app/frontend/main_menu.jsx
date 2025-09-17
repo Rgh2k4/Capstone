13ab4061e7d12151496531cbad57fdf3460dff25
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2025-09-17 12:49:11 -0600

    Integrate ParkMap component into MainMenu with openoverlay on POI

--- app/frontend/main_menu.jsx
+++ app/frontend/main_menu.jsx
@@ -5,6 +5,7 @@ import ParkDetails from "./park_details";
 import Upload from './upload.jsx';
 import Modal from "./components/Modal";
 import ProfileMenu from "./components/profile_menu";
+import ParkMap from "./parkMap";
 
 export default function MainMenu() {
   const [overlay, setOverlay] = useState(false);
@@ -29,14 +30,19 @@ export default function MainMenu() {
                 <ProfileMenu />
             </header>  
             <section>
-                <button onClick={handleOpenOverlay} className="w-screen h-300">map</button>
+              {/*<button onClick={handleOpenOverlay} className="w-screen h-300">map</button>*/}
+              <ParkMap onPOIClick={handleOpenOverlay} classname="w-screen h-300" />
             </section>
-            <Modal isVisible={overlay} onClose={() => setOverlay(false)}>
+          <Modal isVisible={overlay} onClose={() => setOverlay(false)}>
+            <ParkDetails openButtonUpload={handleOpenUpload} />
+          </Modal>
+
+          {/*<Modal isVisible={overlay} onClose={() => setOverlay(false)}>
                 <ParkDetails openButtonUpload={handleOpenUpload} />
             </Modal>
             <Modal isVisible={upload} onClose={() => setUpload(false)}>
                 <Upload />
-            </Modal>
+            </Modal>*/}
         </main>
     );
 
