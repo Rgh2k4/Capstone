fb0213fa8863b18f6cccf365dc37013d8a03a7f8
Amani Bera <141461158+amanibera-sait@users.noreply.github.com>
2025-09-22 17:24:59 -0600

    Add handleSignUp function to switch to SignupPage and update initial state in Home component.

--- app/page.js
+++ app/page.js
@@ -2,14 +2,24 @@
 import { useState } from 'react';
 import Login from './frontend/login.jsx';
 import MainMenu from './frontend/main_menu.jsx';
+import SignupPage from './frontend/signup.jsx';
 
 export default function Home() {
 
-  const [pageScreen, setPageScreen] = useState(<Login handleLogin={handleLogin}/>);
+  const [pageScreen, setPageScreen] = useState(<Login handleLogin={handleLogin} handleSignUp={handleSignUp} />);
 
   function handleLogin() {
     setPageScreen(<MainMenu />);
   }
+
+  function handleSignUp() {
+    setPageScreen(<SignupPage handleNewAccount={handleCompleteSignUp} />);
+  }
+
+  function handleCompleteSignUp() {
+    setPageScreen(<MainMenu />);
+  }
+
 
   return (
     <div>
