c9c79996351a528ab804c7f7d8794eea24edcee2
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-09-23 18:13:10 -0600

    Refactor ProfileWindow layout and remove unused labels.

--- app/frontend/components/profile/profile_window.jsx
+++ app/frontend/components/profile/profile_window.jsx
@@ -4,21 +4,18 @@ function ProfileWindow() {
 
   return (
     <div className=" w-full p-12 rounded flex flex-col justify-center text-center space-y-24">
-      <div className="flex flex-row justify-center text-center">
+      <div className="flex flex-row justify-center text-center space-x-6 align-middle">
         <img
           className="w-50 h-50 bg-gray-400 rounded-full"
           alt="profile picture"
         />
-        <label
-          value="username"
-          className="input"
-        />
       </div>
       <div className="flex flex-col space-y-6">
-        <label
-          value="email"
-          className="input"
-        />
+        <div className=" space-x-6">
+          <input className="input" type="text" placeholder="Enter display name..." value="Display Name"/>
+          <button>Change display name</button>
+        </div>
+        <input className="input" type="text" value="Email" readOnly/>
         <p className=" mt-6 text-1xl">Acoount Created: "dateCreated"</p>
       </div>
     </div>
