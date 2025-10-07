94e453f9fb504bde2c5327dacc650b6f0572ebd2
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-10-07 00:16:37 -0600

    Implemented the admins and users from firestore to the list components.

--- app/frontend/components/admin/accounts/admin_list.jsx
+++ app/frontend/components/admin/accounts/admin_list.jsx
@@ -1,5 +1,7 @@
+import { Button } from "@mantine/core";
+
 export default function AdminList({ handleEdit, data }) {
-  let accounts = data.filter((acc) => acc.role !== "admin");
+  const accounts = data;
 
   function handleData({ admin }) {
     console.log("Data:");
@@ -13,12 +15,26 @@ export default function AdminList({ handleEdit, data }) {
       {accounts.map((admin, index) => (
         <div key={index} className="border-b-4 border-gray-300">
           <div className="grid grid-cols-4 gap-4 m-4">
-            <p className=" mt-6 mr-8 font-bold text-1xl">{admin.id}</p>
+            <p className=" mt-6 mr-8 font-bold text-1xl">{admin.user_ID ? admin.user_ID.slice(0,5) + "...": "N/A"}</p>
             <p className=" mt-6 mr-8 font-semibold text-2xl wrap-anywhere">
-              {admin.username}
+              {admin.email}
             </p>
-            <p className=" mt-6 mr-8 text-1xl">Created: {admin.dateCreated}</p>
-            <button onClick={() => handleData({ admin })}>Edit</button>
+            <p className=" mt-6 mr-8 text-1xl">
+              Created:{" "}
+              {admin.dateCreated
+                ? new Date(
+                    admin.dateCreated.seconds * 1000
+                  ).toLocaleDateString()
+                : "Unknown"}
+            </p>
+            <Button
+              className="w-full"
+              size="lg"
+              variant="filled"
+              onClick={() => handleData({ admin })}
+            >
+              Edit
+            </Button>
           </div>
         </div>
       ))}
