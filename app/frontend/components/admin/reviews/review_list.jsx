7d8925d7dc40340a45a9d7a00640a1e2e861b9fd
Amani Bera <141461158+amanibera-sait@users.noreply.github.com>
2025-10-15 13:14:55 -0600

    Removed bg color in review_list.jsx

--- app/frontend/components/admin/reviews/review_list.jsx
+++ app/frontend/components/admin/reviews/review_list.jsx
@@ -9,7 +9,7 @@ function ReviewList({ setShowModal, sendUser, reviews, showHeader }) {
   };
 
   return (
-    <section className=" bg-gray-100 drop-shadow-md drop-shadow-gray-400">
+    <section className=" drop-shadow-md drop-shadow-gray-400">
       {showHeader && (
         <div className="flex justify-center text-4xl font-bold bg-gray-300 p-4 space-y-4 overflow-y-auto drop-shadow-sm drop-shadow-gray-400">
           <p>Reviews</p>
