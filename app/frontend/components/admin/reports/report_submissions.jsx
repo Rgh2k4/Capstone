d488ffacdd712fd57e518d53512ae2f7d353f05c
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-10-28 17:57:55 -0600

    feat: update report and review submissions layout to include user ID and display name

--- app/frontend/components/admin/reports/report_submissions.jsx
+++ app/frontend/components/admin/reports/report_submissions.jsx
@@ -11,9 +11,10 @@ function ReportSubmissions({ handleReport, reports }) {
     <ul>
       {reports.map((rep, index) => (
         <div key={index} className="border-b-4 border-gray-300">
-          <div className="grid grid-cols-3 gap-4 m-4">
-            <p className=" mt-6 mr-8 font-semibold text-2xl">{rep.username}</p>
-            <p className=" mt-6 mr-8 text-1xl">Submited: {rep.date}</p>
+          <div className="grid grid-cols-4 gap-4 m-4">
+            <p className=" mt-6 mr-8 font-bold text-1xl">{rep.uid ? rep.uid.slice(0,5) + "...": "N/A"}</p>
+            <p className=" mt-6 mr-8 font-semibold text-2xl">{rep.displayName || "Anonymous"}</p>
+            <p className=" mt-6 mr-8 text-1xl">Submited: {/*rep.date*/}</p>
             <Button
               className="w-full"
               size="lg"
--- app/frontend/components/admin/reviews/review_submissions.jsx
+++ app/frontend/components/admin/reviews/review_submissions.jsx
@@ -12,8 +12,9 @@ function ReviewSubmissions({ handleReview, reviews }) {
     <ul>
       {reviews.map((rev, index) => (
         <div key={index} className="border-b-4 border-gray-300">
-          <div className="grid grid-cols-3 gap-4 m-4">
-            <p className=" mt-6 mr-8 font-semibold text-2xl">{rev.username}</p>
+          <div className="grid grid-cols-4 gap-4 m-4">
+            <p className=" mt-6 mr-8 font-bold text-1xl">{rev.uid ? rev.uid.slice(0,5) + "...": "N/A"}</p>
+            <p className=" mt-6 mr-8 font-semibold text-2xl">{rev.displayName || "Anonymous"}</p>
             <p className=" mt-6 mr-8 text-1xl">Submited: {rev.date}</p>
             <Button
               className="w-full"
