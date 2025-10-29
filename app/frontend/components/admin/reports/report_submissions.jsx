e7caa9b0aab9e0fc7ee59487485ab9741522637f
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2025-10-29 09:56:04 -0600

    feat: update report handling in ReportWindow and ReportSubmissions components

--- app/frontend/components/admin/reports/report_submissions.jsx
+++ app/frontend/components/admin/reports/report_submissions.jsx
@@ -12,9 +12,9 @@ function ReportSubmissions({ handleReport, reports }) {
       {reports.map((rep, index) => (
         <div key={index} className="border-b-4 border-gray-300">
           <div className="grid grid-cols-4 gap-4 m-4">
-            <p className=" mt-6 mr-8 font-bold text-1xl">{rep.uid ? rep.uid.slice(0,5) + "...": "N/A"}</p>
-            <p className=" mt-6 mr-8 font-semibold text-2xl">{rep.displayName || "Anonymous"}</p>
-            <p className=" mt-6 mr-8 text-1xl">Submited: {/*rep.date*/}</p>
+            <p className=" mt-6 mr-8 font-bold text-1xl">{rep.reportedUserID ? rep.reportedUserID.slice(0,5) + "...": "N/A"}</p>
+            <p className=" mt-6 mr-8 font-semibold text-2xl">{rep.reason}</p>
+            <p className=" mt-6 mr-8 text-1xl">Submited: {rep.dateReported}</p>
             <Button
               className="w-full"
               size="lg"
